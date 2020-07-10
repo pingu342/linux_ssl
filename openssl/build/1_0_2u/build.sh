49c809c3d74fec60eec05e6effeb9ba15fecaa1c
ping342 <pingu342@users.noreply.github.com>
2020-07-10 10:09:10 +0000

    initial

---
+++ openssl/build/1_0_2u/build.sh
@@ -0,0 +1,18 @@
+TOP_PATH=$(cd ../../ && pwd)
+INSTALL_PATH=$TOP_PATH/1_0_2u
+echo INSTALL_PATH: $INSTALL_PATH
+
+if [ ! -e OpenSSL_1_0_2u.tar.gz ]; then
+	wget https://github.com/openssl/openssl/archive/OpenSSL_1_0_2u.tar.gz
+fi
+
+rm -rf $INSTALL_PATH
+rm -rf openssh-OpenSSL_1_0_2u
+tar xvzf OpenSSL_1_0_2u.tar.gz
+
+pushd openssl-OpenSSL_1_0_2u
+./config --prefix=$INSTALL_PATH --openssldir=$INSTALL_PATH/openssl shared
+make
+make install
+popd
+
