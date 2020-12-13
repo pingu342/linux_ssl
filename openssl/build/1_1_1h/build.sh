b6f374c5e4504cd79706d6afbaa235b81b383cf9
pingu342 <pingu342@users.noreply.github.com>
2020-12-13 13:06:29 +0900

    add openssl-1.1.1h

---
+++ openssl/build/1_1_1h/build.sh
@@ -0,0 +1,18 @@
+TOP_PATH=$(cd ../../ && pwd)
+INSTALL_PATH=$TOP_PATH/1_1_1h
+echo INSTALL_PATH: $INSTALL_PATH
+
+if [ ! -e openssl-1.1.1h.tar.gz ]; then
+	wget https://www.openssl.org/source/openssl-1.1.1h.tar.gz
+fi
+
+rm -rf $INSTALL_PATH
+rm -rf openssl-1.1.1h
+tar xvzf openssl-1.1.1h.tar.gz
+
+pushd openssl-1.1.1h
+./config --prefix=$INSTALL_PATH --openssldir=$INSTALL_PATH/openssl shared
+make
+make install
+popd
+
