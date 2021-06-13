8659522aa202d821c50f84d35e445d3f65bebbe2
pingu342 <pingu342@users.noreply.github.com>
2021-06-13 20:38:29 +0900

    ADD --enable-sp-math-all=small

--- wolfssl/build/v4.3.0/build.sh
+++ wolfssl/build/v4.3.0/build.sh
@@ -1,5 +1,6 @@
 TOP_PATH=$(cd ../../ && pwd)
-INSTALL_PATH=$TOP_PATH/v4.3.0
+#INSTALL_PATH=$TOP_PATH/v4.3.0
+INSTALL_PATH=$TOP_PATH/v4.3.0_sp-math-all-small
 echo INSTALL_PATH: $INSTALL_PATH
 
 if [ ! -e v4.3.0-stable.tar.gz ]; then
@@ -12,10 +13,11 @@ tar xvzf v4.3.0-stable.tar.gz
 
 pushd wolfssl-4.3.0-stable
 ./autogen.sh
-./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-tls13 --enable-asm CFLAGS="-march=armv7-a -mthumb"
-#./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-tls13 --enable-asm --enable-sp --enable-sp-asm CFLAGS="-march=armv7-a -mthumb"
-#./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-tls13 --enable-asm --enable-sp --enable-sp-math-all --enable-sp-asm CFLAGS="-march=armv7-a -mthumb"
-#./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-tls13 --enable-asm --enable-sp --enable-sp-math --enable-sp-asm CFLAGS="-march=armv7-a -mthumb"
+#./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-tls13 --enable-asm CFLAGS="-march=armv7-a -mthumb -Os"
+#./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-tls13 --enable-asm --enable-sp --enable-sp-asm CFLAGS="-march=armv7-a -mthumb -Os"
+#./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-tls13 --enable-asm --enable-sp --enable-sp-math-all --enable-sp-asm CFLAGS="-march=armv7-a -mthumb -Os"
+./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-tls13 --enable-asm --enable-sp --enable-sp-math-all=small --enable-sp-asm CFLAGS="-march=armv7-a -mthumb -Os"
+#./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-tls13 --enable-asm --enable-sp --enable-sp-math --enable-sp-asm CFLAGS="-march=armv7-a -mthumb -Os"
 make
 make install
 popd
--- wolfssl/build/v4.7.0/build.sh
+++ wolfssl/build/v4.7.0/build.sh
@@ -1,8 +1,10 @@
 TOP_PATH=$(cd ../../ && pwd)
-INSTALL_PATH=$TOP_PATH/v4.7.0
+#INSTALL_PATH=$TOP_PATH/v4.7.0
 #INSTALL_PATH=$TOP_PATH/v4.7.0_sp
 #INSTALL_PATH=$TOP_PATH/v4.7.0_sp-math-all
+INSTALL_PATH=$TOP_PATH/v4.7.0_sp-math-all-small
 #INSTALL_PATH=$TOP_PATH/v4.7.0_sp-math
+#INSTALL_PATH=$TOP_PATH/v4.7.0_nofastmath
 echo INSTALL_PATH: $INSTALL_PATH
 
 if [ ! -e v4.7.0-stable.tar.gz ]; then
@@ -15,10 +17,13 @@ tar xvzf v4.7.0-stable.tar.gz
 
 pushd wolfssl-4.7.0-stable
 ./autogen.sh
-./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-asm CFLAGS="-march=armv7-a -mthumb"
+#./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-asm CFLAGS="-march=armv7-a -mthumb"
 #./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-asm --enable-sp --enable-sp-asm CFLAGS="-march=armv7-a -mthumb"
-#./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-asm --enable-sp --enable-sp-math-all --enable-sp-asm CFLAGS="-march=armv7-a -mthumb"
+#./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-asm --enable-sp --enable-sp-math-all --enable-sp-asm CFLAGS="-march=armv7-a -mthumb -Os"
+./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-asm --enable-sp --enable-sp-math-all=small --enable-sp-asm CFLAGS="-march=armv7-a -mthumb -Os"
 #./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-asm --enable-sp --enable-sp-math --enable-sp-asm CFLAGS="-march=armv7-a -mthumb"
+#./configure --prefix=$INSTALL_PATH --enable-keygen --enable-aesccm --enable-asm --disable-fastmath CFLAGS="-march=armv7-a -mthumb"
+
 make
 make install
 popd
